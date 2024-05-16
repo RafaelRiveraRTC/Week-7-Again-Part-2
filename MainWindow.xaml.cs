63064843f8987ea25ebdf239107072d5bb501223
Emiliano <emilianovolk75@gmail.com>
2024-05-15 19:45:45 -0700

    Update this is a personal func project for now could possibly mess with this later

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -19,6 +19,38 @@ namespace Week_7_Again_Part_2
         public MainWindow()
         {
             InitializeComponent();
+
+            new NewControl();
+
+            new This_Is_My_Stuff();
+
+            MessageBox.Show(canvasMain.Children.Count.ToString());
+            
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+
+
+
+            userControlNewControl.Visibility =
+                Visibility.Hidden;
+
+            MessageBox.Show(userControlNewControl.IsVisible.ToString());
+            Thread.Sleep(5000);//This will cause a 5 second delay
+            userControlNewControl.Visibility = Visibility.Visible;//This makes it so it will immediately appear right after the button is clicked
+
+
+        }
+
+        private void TheCoolerButton_Click(object sender, RoutedEventArgs e)
+        {
+            RichardLikesTrains.Visibility =
+               Visibility.Hidden;
+
+            MessageBox.Show(RichardLikesTrains.IsVisible.ToString());
+            Thread.Sleep(5000);//This will cause a 5 second delay
+            RichardLikesTrains.Visibility = Visibility.Visible;//This makes it so it will immediately appear right after the button is clicked
         }
     }
 }
--- README.md
+++ README.md
@@ -1,1 +1,21 @@
 # Week 7 Again Part 2
+
+Wwhat do we want to happen to our box when we click the button
+
+if the button is visible we want it hidden,else we want it to to be revealed
+
+You gotta make the new user control
+
+we are going to add new buttons
+
+1 create a new user control,name of my choice
+
+2 add 2 new buttons to our main window xaml
+
+3 add fucntionality
+	when you click one button it will show one user control
+	when you click the other it will show the second user control
+You should have no default user controls in your developer window
+
+
+i was able to create a second button with the 5 second sleep delay for both
